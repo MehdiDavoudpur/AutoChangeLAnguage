6d11e94c6b95599164b40864a806d8215a0c0b21
davoudpur <davoudpour2000@gmail.com>
2024-03-16 20:09:35 +0330

    last title strategy is created set title is created & appearance is improved &

--- main.py
+++ main.py
@@ -5,41 +5,43 @@ import pyautogui
 
 # new_app_title is necessary for checking  if app title is changed or not.
 new_app_title = ''
-# title_list is necessary for checking being repetitive in a for loop to add to title_list_string.
-title_list = []
 # title_list_string is necessary for printing out to user.
-title_list_string = ''
+last_title = ''
+persian_titles = []
 
 
 def background_task():
     while True:
         global new_app_title
-        global title_list
-        global title_list_string
+        global last_title
         # get active app title as app_title.
         app_title = pyautogui.getActiveWindowTitle()
         # check if app title is changed or not.
         if app_title != new_app_title:
             # check if app title is changed from  "کافه اینترنشنال" to other thing. new app title here is previous
             # app title really.
-            i = 0
+            '''i = 0
             for title in title_list:
                 if title == app_title:
                     i = 1
-            if i == 0:
-                # app title is not repetitious and is appended to title_list.
-                title_list.append(app_title)
+            if i == 0:'''
+            # app title is not repetitious and is appended to title_list.
+            if app_title != 'Auto Change Language':
                 if app_title:
-                    title_list_string = title_list_string + app_title + '\n'
-            # check if app title is changed from  "کافه اینترنشنال" to other thing. new app title here is previous
+                    last_title = app_title
+            # check if app title is changed from  per_title to other thing. new app title here is previous
             # app title really.
-            if "کافه اینترنشنال" in new_app_title:
-                # change language from فا to ENG
-                pyautogui.hotkey('shift', 'alt')
-            # check if app title is changed from other thing to  "کافه اینترنشنال"
-            if "کافه اینترنشنال" in app_title:
-                # change language from ENG to فا
-                pyautogui.hotkey('shift', 'alt')
+
+            for per_title in persian_titles:
+                if per_title in new_app_title:
+                    # change language from PER to ENG
+                    pyautogui.hotkey('shift', 'alt')
+                # check if app title is changed from other thing to  per_title
+                if app_title:
+                    if per_title in app_title:
+                        # change language from ENG to PER
+                        pyautogui.hotkey('shift', 'alt')
+
             # refresh app title
             new_app_title = app_title
 
@@ -53,22 +55,46 @@ background_thread.daemon = True
 background_thread.start()
 
 
-# method for action of clicking
-def click():
-    label.config(text=title_list_string)
+# method for action of clicking on get title button.
+def get_click():
+    text.delete(1.0, 'end')
+    text.insert('end', last_title)
 
+
+# method for action of clicking on set title button.
+def set_click():
+    persian_titles.append(last_title)
+
+
+'''def on_text_scroll(*args):
+    text1.yview(*args)'''
 
 # Create the main application window
 root = tk.Tk()
-root.title("Simple PyGUI App")
-
-# Create a label widget
-label = tk.Label(root, )
-label.pack(pady=5, padx=10)
+root.title("Auto Change Language")
 
 # Create a button widget
-button = tk.Button(root, text="Capture", command=click)
-button.pack(pady=5, padx=10)
+button = tk.Button(root, text="Get Title", command=get_click)
+button.grid(row=0, column=1, pady=5)
+
+# Create a label widget
+label = tk.Label(root, text='App Titles:')
+label.grid(row=1, column=0, padx=(20, 2), pady=(5, 5))
+
+# Create a text widget
+text = tk.Text(root, height=1)
+text.grid(row=1, column=1, padx=(2, 20), pady=(5, 5))
+
+'''# Create a Scrollbar widget
+scrollbar = tk.Scrollbar(root, command=on_text_scroll)
+scrollbar.grid(row=1, column=2, )
+
+# Link Text widget and Scrollbar widget
+text1.config(yscrollcommand=scrollbar.set)'''
+
+# Create a button widget
+button2 = tk.Button(root, text="Set Title", command=set_click)
+button2.grid(row=3, column=1, pady=5)
 
 # Run the Tkinter event loop
 root.mainloop()
