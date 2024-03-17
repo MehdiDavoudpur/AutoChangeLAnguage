ac5e9071b1e0f67d063d325b4acf8404a317db7a
davoudpur <davoudpour2000@gmail.com>
2024-03-17 18:02:35 +0330

    database is created, show from db is implemented, ui is developed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import threading
 import time
 import pyautogui
+import database
 
 # new_app_title is necessary for checking  if app title is changed or not.
 new_app_title = ''
@@ -50,34 +51,110 @@ background_thread.start()
 
 # method for action of clicking on get title button.
 def get_click():
-    text.delete(1.0, 'end')
-    text.insert('end', last_title)
+    txt1.delete(1.0, 'end')
+    txt1.insert('end', last_title)
 
 
 # method for action of clicking on set title button.
 def set_click():
     persian_titles.append(last_title)
+    database.insert(last_title)
+
+
+def show_click():
+    all_set_titles = database.show()
+    print(all_set_titles)
+    txt2.delete(1.0, 'end')
+    txt2.insert('end', all_set_titles)
 
 
 # Create the main application window
 root = tk.Tk()
 root.title("Auto Change Language")
 
-# Create a button widget
-button = tk.Button(root, text="Get Title", command=get_click)
-button.grid(row=0, column=1, pady=5)
 
-# Create a label widget
-label = tk.Label(root, text='App Titles:')
-label.grid(row=1, column=0, padx=(20, 2), pady=(5, 5))
+def resize(event):
+    # Adjust the widget size when the window is resized
+    canvas.config(width=event.width, height=event.height)
 
-# Create a text widget
-text = tk.Text(root, height=1)
-text.grid(row=1, column=1, padx=(2, 20), pady=(5, 5))
 
-# Create a button widget
-button2 = tk.Button(root, text="Set Title", command=set_click)
-button2.grid(row=3, column=1, pady=5)
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
 
-# Run the Tkinter event loop
+
+def widget_block_1(parent):
+    # Create a frame to hold the widgets
+    frame = tk.Frame(parent, borderwidth=2, relief="ridge")
+
+    # Add some widgets to the frame
+    # Create button widgets
+    btn1 = tk.Button(frame, text="Get", command=get_click)
+    btn2 = tk.Button(frame, text="Set", command=get_click)
+    btn3 = tk.Button(frame, text="Show", command=show_click)
+    btn4 = tk.Button(frame, text="Delete", command=get_click)
+
+    # Use pack() to organize the widgets in the frame
+    btn1.grid(row=0, column=0, padx=2)
+    btn2.grid(row=0, column=1, padx=2)
+    btn3.grid(row=0, column=2, padx=2)
+    btn4.grid(row=0, column=3, padx=2)
+
+    return frame
+
+
+# Create multiple blocks of widgets
+block1 = widget_block_1(root)
+
+# Use grid() to organize the blocks in rows and columns
+block1.grid(row=0, column=1, padx=10, pady=10)
+
+
+def widget_block_2(parent):
+    # Create a frame to hold the widgets
+    frame = tk.Frame(parent, borderwidth=2, relief="ridge")
+
+    # Add some widgets to the frame
+    # Create button widgets
+    btn1 = tk.Button(frame, text="Delete All", command=get_click)
+    btn2 = tk.Button(frame, text="Exit", command=get_click)
+
+    # Use pack() to organize the widgets in the frame
+    btn1.grid(row=0, column=0, padx=(2, 30))
+    btn2.grid(row=0, column=1, padx=(30, 2))
+
+    return frame
+
+
+# row 1
+lbl1 = tk.Label(root, text='Active App Title:')
+lbl1.grid(row=1, column=0, padx=(20, 2), pady=(5, 5), sticky='e')
+
+txt1 = tk.Text(root, height=1, width=50)
+txt1.grid(row=1, column=1, padx=(2, 2), pady=(5, 5))
+
+# row 2
+lbl2 = tk.Label(root, text='All Set Titles:')
+lbl2.grid(row=2, column=0, padx=(20, 2), pady=(5, 5), sticky='e')
+
+
+def on_text_scroll(*args):
+    txt2.yview(*args)
+
+
+txt2 = tk.Text(root, height=25, width=50)
+txt2.grid(row=2, column=1, padx=(2, 2), pady=(5, 5), sticky="ns")
+
+# Create a Scrollbar widget
+scrollbar = tk.Scrollbar(root, command=on_text_scroll)
+scrollbar.grid(row=2, column=2, sticky="ns")
+
+# row 3
+block2 = widget_block_2(root)
+block2.grid(row=3, column=1, padx=10, pady=10)
+
+canvas = tk.Canvas(root, bg="white")
+canvas.pack(fill=tk.BOTH, expand=True)
+
+canvas.bind("<Configure>", resize)
+
 root.mainloop()
